b206929e8b8ca750d5142f8671cc079770ba70c7
sh03415 <s22100512@walab.handong.edu>
2022-05-09 18:02:17 +0900

    update movie.h

--- movie.h
+++ movie.h
@@ -12,10 +12,7 @@ typedef struct{
 
 int createMovie(Movie *m); // 영화 정보 추가
 void readMovie(Movie m); // 하나의 영화 정보 출력
-void listMovie(Movie *m, int count); // 모든 영화 출력
 int updateMovie(Movie *m); // 영화 하나 수정
 int deleteMovie(Movie *m); // 영화 하나 삭제
-int selectMenu() // 메뉴 선택
-int selectDataNo(Movie *m, int count); // 번호 선택
 
 
