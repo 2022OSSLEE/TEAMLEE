707dd85b916ed376e5fc279c678a64edde170ba0
FLOEOI\SAMSUNG <yerin@handong.ac.kr>
2022-05-09 18:54:27 +0900

    modify movie.c

--- movie.c
+++ movie.c
@@ -2,15 +2,21 @@
 
 int createMovie(Movie *m) {
     printf("제목: ");
-    scanf("%s", m->name);
+    getchar();
+    fgets(m->name, 100, stdin);
+    m->name[strlen(m->name)-1] = '\0';
     printf("감독: ");
-    scanf("%s", m->gamdog);
+    getchar();
+    fgets(m->gamdog, 100, stdin);
+    m->gamdog[strlen(m->gamdog)-1] = '\0';
     printf("장르: ");
-    scanf("%s", m->type);
-    printf("제작국가: ");
+    fgets(m->type, 100, stdin);
+    m->type[strlen(m->type)-1] = '\0';
+    printf("제작국가(1: 한국영화, 2: 외국영화) : ");
     scanf("%d", &m->c);
     printf("한줄평: ");
-    printf("%s", m->intro);  
+    getchar();
+    m->intro[strlen(m->intro)-1] = '\0';
 
     printf("=>추가됨!\n");
 
@@ -19,16 +25,22 @@ int createMovie(Movie *m) {
 
 int updateMovie(Movie *m) {
     printf("제목: ");
-    scanf("%s", m->name);
+    getchar();
+    fgets(m->name, 100, stdin);
+    m->name[strlen(m->name)-1] = '\0';
     printf("감독: ");
-    scanf("%s", m->gamdog);
+    getchar();
+    fgets(m->gamdog, 100, stdin);
+    m->gamdog[strlen(m->gamdog)-1] = '\0';
     printf("장르: ");
-    scanf("%s", m->type);
-    printf("제작국가: ");
+    fgets(m->type, 100, stdin);
+    m->type[strlen(m->type)-1] = '\0';
+    printf("제작국가(1: 한국영화, 2: 외국영화) : ");
     scanf("%d", &m->c);
     printf("한줄평: ");
-    printf("%s", m->intro);
-
+    getchar();
+    m->intro[strlen(m->intro)-1] = '\0';
+    
     printf("=> 수정됨!\n");
 
     return 0;
