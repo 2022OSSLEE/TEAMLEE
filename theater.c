ff62b321d074db58a343e5a238d379f5f489e2ca
sh03415 <s22100512@walab.handong.edu>
2022-05-09 20:23:31 +0900

    update theater.c

--- theater.c
+++ theater.c
@@ -8,34 +8,34 @@ int main(void){
     int curcount=0;
     int count = 0, menu; 
 
-    while(1) {
-        menu = selectMenu();
-        if(menu == 0) break;
-	else if(menu == 1) listMovie(mlist,curcount); 
-        else if (menu == 2) count+=createMovie(&mlist[curcount++]); 
-        else if (menu == 3) {
-            int no = selectDataNo(mlist, curcount);
-            if(no == 0){
-                printf("=>취소됨!");
-                continue;
-            }
-            updateMovie(&mlist[no-1]);
-        }
-        else if (menu == 4) {
-            int no = selectDataNo(mlist, curcount);
-            if(no == 0){
-                printf("=>취소됨!");
-                continue;
-            }
-            int deleteok;
-            printf("정말로 삭제하시겠습니까?(삭제:1)");
-            scanf("%d", &deleteok);
-            if(deleteok == 1) {
-                if(deleteMovie(&mlist[no-1])) count --;       
-            } 
-	}
+	while(1) {
+    		menu = selectMenu();
+        	if(menu == 0) break;
+		else if(menu == 1) listMovie(mlist,curcount); 
+        	else if (menu == 2) count+=createMovie(&mlist[curcount++]); 
+        	else if (menu == 3) {
+            		int no = selectDataNo(mlist, curcount);
+            		if(no == 0){
+                		printf("=>취소됨!");
+                		continue;
+            		}
+            		updateMovie(&mlist[no-1]);
+        	}	
+        	else if (menu == 4) {
+            		int no = selectDataNo(mlist, curcount);
+            		if(no == 0){
+                		printf("=>취소됨!");
+                		continue;
+            		}
+            		int deleteok;
+            		printf("정말로 삭제하시겠습니까?(삭제:1)");
+            		scanf("%d", &deleteok);
+            		if(deleteok == 1) {
+                		if(deleteMovie(&mlist[no-1])) count --;       
+            		}		 
+		}
 
-	printf("\n종료됨!\n");
-   	return 0;
-    }
+		printf("\n종료됨!\n");
+	}
+	return 0;
 }
